e94e544b9a33b320ec5c84682fe3cf70ffd2a97a
Bruno Ochotorena <hackerhilt@gmail.com>
2024-01-05 03:06:54 -0300

    More updates

--- build.rs
+++ build.rs
@@ -1,6 +1,14 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("grpc-interfaces/cartesi-machine-checkin.proto")?;
-    tonic_build::compile_protos("grpc-interfaces/cartesi-machine.proto")?;
+    // tonic_build::compile_protos("grpc-interfaces/versioning.proto")?;
+
+    tonic_build::configure().build_server(false).compile(
+        &[
+            "grpc-interfaces/stateserver.proto",
+            "grpc-interfaces/versioning.proto",
+            "grpc-interfaces/cartesi-machine.proto",
+        ],
+        &["grpc-interfaces"],
+    )?;
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("hello world");
+use cartesi_machine::machine_client::MachineClient;
+use server_state::GetStateRequest;
+use tonic::{transport::Channel, Request};
+
+pub mod versioning {
+    tonic::include_proto!("versioning");
+}
+
+pub mod server_state {
+    tonic::include_proto!("state_server");
+}
+
+pub mod cartesi_machine {
+    tonic::include_proto!("cartesi_machine");
+}
+
+// https://github.com/hyperium/tonic/blob/master/examples/helloworld-tutorial.md
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let machine = MachineClient::connect("http://[::1]:50051").await?;
+
+    println!("Connected");
+
+    let response = Request::new(GetStateRequest {
+        json_initial_state: String::from("{}"),
+    });
+
+    println!("Requesting state {:?}", response);
+
+
+    // let response = client
+    //     .get_status(request)
+    //     .await?
+    //     .into_inner();
+
+    // println!("RESPONSE={:?}", response);
+
+    Ok(())
 }
